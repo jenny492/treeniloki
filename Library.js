8db46d33ae2bbf1d3eecd7e3b02013e16593d13c
Jenny <205511984+jenny492@users.noreply.github.com>
2026-04-01 18:19:06 +0300

    Asennettu kirjastot tab-navigoinnille ja tehty näkymät Home ja Library.

---
+++ Library.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function Library() {
+    
+    return (
+        <View style={styles.container}>
+            <Text>Library</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
