84cb366c0c8d27aad9b6a7c7dca8d9c448b015f9
Jenny <205511984+jenny492@users.noreply.github.com>
2026-04-03 17:48:20 +0300

    Revert "Aloitettu tietokannan teko. Tehty taulujen luonti."
    This reverts commit 93100173e50aaaf90a1f27c1262e866ddd2374e4.

--- App.js
+++ App.js
@@ -4,17 +4,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './Home';
 import Library from './Library';
-import { Icon, MD3Colors } from 'react-native-paper';
+import { Icon } from 'react-native-paper';
 import { PaperProvider } from 'react-native-paper';
-import { useEffect } from 'react';
-import { initialize } from './Database';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-
-  useEffect(() => { initialize(); }, []);
-
   return (
     <PaperProvider>
       <NavigationContainer>
@@ -30,7 +25,7 @@ export default function App() {
               } else if (route.name === 'Liikepankki') {
                 iconName = focused ? 'weight-lifter' : 'weight-lifter';
               }
-              return <Icon source={iconName} color={MD3Colors.primary50} size={30} />;
+              return <Icon source={iconName} size={30} />;
             }
           })}>
           <Tab.Screen name="Koti" component={Home} />
